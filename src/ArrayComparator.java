01aed4de5ecf25da40985ee5311269bffa2e2d5d
Marcin <Majsyn@gmail.com>
2018-07-23 10:48:17 +0200

    v 1.1

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -4,10 +4,8 @@ public class ArrayComparator {
 
     boolean compare(int tab1[], int tab2[]) {
         if (tab1.length == tab2.length) {
-            for (int i = 0; i < tab1.length; i++) {
-                if (Arrays.equals(tab1, tab2))
-                    return true;
-            }
+            if (Arrays.equals(tab1, tab2))
+                return true;
         }
         return false;
     }
